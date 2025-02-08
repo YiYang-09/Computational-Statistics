dea2030b0d6afd7717ff025bacfd2a424eed9094
Helena Llorens Lluís <helenallorens14@gmail.com>
2025-02-08 22:56:14 +0100

    Question 2 finished

--- lab3/lab3_ex2.R
+++ lab3/lab3_ex2.R
@@ -1,5 +1,6 @@
-# a
+library(ggplot2)
 
+# a
 
 n <- 10000000
 mu <- c(0, 0)
@@ -13,16 +14,27 @@ box_muller_normal <- function(n, mu, sigma){
   
   x1 <- sqrt(-2*log(u))*cos(2*pi*v)*sqrt(sigma[1, 1]) + mu[1]
   x2 <- sqrt(-2*log(u))*sin(2*pi*v)*sqrt(sigma[2, 2]) + mu[2]
-  
-  proc.time()
 
-  return(c(x1, x2))
+  return(data.frame(x1, x2))
 }
 
+start_time <- proc.time()
 dist <- box_muller_normal(n, mu, sigma)
-proc.time()
+end_time <- proc.time()
 
-plot(density(dist))
+print(end_time - start_time)
+
+system.time({
+  dist <- box_muller_normal(n, mu, sigma)
+})
+
+
+# ggplot(dist, aes(x = x1, y = x2)) +
+#   geom_point(alpha = 0.2) +
+#   theme_minimal() +
+#   ggtitle("Bivariate Normal Distribution") +
+#   xlab("X1") + 
+#   ylab("X2")
 
 
 # b
@@ -31,22 +43,38 @@ library(mvtnorm)
 
 set.seed(1234)
 dist2 <- rmvnorm(n, mean = mu, sigma = sigma)
-proc.time()
 
-plot(density(dist2))
+system.time({
+  dist2 <- rmvnorm(n, mean = mu, sigma = sigma)
+})
+
+# plot(density(dist2))
 
 
 # c
 
 mu1 <- c(0, 0)
 mu2 <- c(1.2, 1.5)
+mu <- list(mu1, mu2)
 
 sigma1 <- matrix(c(0.6, 0, 0, 0.6), nrow = 2)
 sigma2 <- matrix(c(0.5, 0, 0, 0.5), nrow = 2)
+sigma <- list(sigma1, sigma2)
 
 prob <- c(0.5, 0.5)
 n <- 1000
 
+x <- data.frame(matrix(NA, nrow = n, ncol = 3))
+set.seed(1234)
+for(i in 1:n){
+  g <- sample(1:2, 1, replace = TRUE, p = prob)
+  x[i, 1:2] <- rmvnorm(n = 1, mean = mu[[g]], sigma = sigma[[g]])
+  x[i, 3] <- as.character(g)
+}
 
-g <- sample(length(mu), n, replace=TRUE, p=prob)
-x <- rnorm(n, mean = mu[g], sd = sigma[g])
+ggplot(x, aes(x = x[, 1], y = x[, 2], color = x[, 3])) +
+  geom_point(alpha = 0.6) +
+  theme_minimal() +
+  scale_color_manual(values = c("#4E79A7", "#E15759")) + 
+  labs(title = "Bivariate Normal Mixture", x = "X1", y = "X2", color = "Original distribution") + 
+  theme(legend.position="bottom")
